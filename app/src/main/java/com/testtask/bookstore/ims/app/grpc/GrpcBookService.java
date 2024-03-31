31c963b892fccb5c32511b0ce5dbf44c2a1d2687
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-03-31 22:42:18 +0300

    improved proto file

--- app/src/main/java/com/testtask/bookstore/ims/app/grpc/GrpcBookService.java
+++ app/src/main/java/com/testtask/bookstore/ims/app/grpc/GrpcBookService.java
@@ -1,5 +1,24 @@
 package com.testtask.bookstore.ims.app.grpc;
 
-public class GrpcBookService {
+import com.testtask.bookstore.ims.app.service.BookServiceImpl;
+import com.testtask.bookstore.ims.proto.BookManagementServiceGrpc.BookManagementServiceImplBase;
+import com.testtask.bookstore.ims.proto.BookResponse;
+import com.testtask.bookstore.ims.proto.CreateBookRequest;
+import io.grpc.stub.StreamObserver;
 
+//@GrpcService
+public class GrpcBookService extends BookManagementServiceImplBase {
+private BookServiceImpl bookService;
+    
+    @Override
+    public void createBook(CreateBookRequest request, StreamObserver<BookResponse> responseObserver) {
+        BookResponse response = BookResponse.newBuilder()
+                .setAuthor(request.getAuthor())
+                .setTitle(request.getTitle())
+                .setId(null).build();
+        responseObserver.onNext(response);// method to send it to the client
+        responseObserver.onCompleted();// call onCompleted() to specify that we’ve finished dealing with the RPC;
+                                       // otherwise, the connection will be hung, and the client will just wait for
+                                       // more information to come in.
+    }
 }
