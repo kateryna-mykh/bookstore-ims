58f674ed5e139ac0aff2427cc00377870f1d70f7
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-04-02 10:47:05 +0300

    implemented @GrpcService for book management

--- app/src/main/java/com/testtask/bookstore/ims/app/grpc/GrpcBookService.java
+++ app/src/main/java/com/testtask/bookstore/ims/app/grpc/GrpcBookService.java
@@ -1,24 +1,56 @@
 package com.testtask.bookstore.ims.app.grpc;
 
+import com.google.protobuf.Empty;
+import com.testtask.bookstore.ims.app.mapper.BookDtoProtoMapper;
 import com.testtask.bookstore.ims.app.service.BookServiceImpl;
-import com.testtask.bookstore.ims.proto.BookManagementServiceGrpc.BookManagementServiceImplBase;
-import com.testtask.bookstore.ims.proto.BookResponse;
+import com.testtask.bookstore.ims.proto.BookFullObject;
+import com.testtask.bookstore.ims.proto.BookUpdateResponse;
 import com.testtask.bookstore.ims.proto.CreateBookRequest;
-import io.grpc.stub.StreamObserver;
+import com.testtask.bookstore.ims.proto.FindByIdRequest;
+import com.testtask.bookstore.ims.proto.ReactorBookManagementServiceGrpc;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.server.service.GrpcService;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-//@GrpcService
-public class GrpcBookService extends BookManagementServiceImplBase {
-private BookServiceImpl bookService;
-    
+@RequiredArgsConstructor
+@GrpcService
+public class GrpcBookService
+        extends ReactorBookManagementServiceGrpc.BookManagementServiceImplBase {
+    private final BookServiceImpl bookService;
+    private final BookDtoProtoMapper bookProtoMapper;
+
     @Override
-    public void createBook(CreateBookRequest request, StreamObserver<BookResponse> responseObserver) {
-        BookResponse response = BookResponse.newBuilder()
-                .setAuthor(request.getAuthor())
-                .setTitle(request.getTitle())
-                .setId(null).build();
-        responseObserver.onNext(response);// method to send it to the client
-        responseObserver.onCompleted();// call onCompleted() to specify that we’ve finished dealing with the RPC;
-                                       // otherwise, the connection will be hung, and the client will just wait for
-                                       // more information to come in.
+    public Mono<BookFullObject> createBook(Mono<CreateBookRequest> newBook) {
+        return newBook.map(book -> bookProtoMapper.protoCreateToDto(book))
+                .flatMap(bookDto -> bookService.save(bookDto))
+                .map(createdbook -> bookProtoMapper.dtoToProto(createdbook));
+    }
+
+    @Override
+    public Mono<BookUpdateResponse> update(Mono<BookFullObject> book) {
+        return book.map(b -> bookProtoMapper.protoUpdateToDto(b))
+                .flatMap(bookDto -> bookService.update(bookDto))
+                .map(updatedInfo -> bookProtoMapper.dtoToUpdateProto(updatedInfo));
+    }
+
+    @Override
+    public Mono<Empty> delete(Mono<FindByIdRequest> bookId) {
+        bookId.map(id -> UUID.fromString(id.getBookId()))
+                .flatMap(uuid -> bookService.deleteById(uuid));
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<BookFullObject> getById(Mono<FindByIdRequest> bookId) {
+        return bookId.map(id -> UUID.fromString(id.getBookId()))
+                .flatMap(uuid -> bookService.findById(uuid))
+                .map(foundBook -> bookProtoMapper.dtoToProto(foundBook));
+    }
+
+    @Override
+    public Flux<BookFullObject> getAllBooks(Mono<Empty> mockedObject) {
+        return bookService.findAll().map(foundBooks -> bookProtoMapper.dtoToProto(foundBooks));
     }
 }
