97fe09e37b8753ee28fc810d9eab490ac8c47c56
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-04-06 15:59:16 +0300

    add test configuaration fix

--- app/src/test/java/com/testtask/bookstore/ims/app/ApplicationTest.java
+++ app/src/test/java/com/testtask/bookstore/ims/app/ApplicationTest.java
@@ -1,11 +1,8 @@
 package com.testtask.bookstore.ims.app;
 
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTest {
-    @Test
-    void contextLoads() {
-    }
+
 }
